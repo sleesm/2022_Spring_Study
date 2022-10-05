7bcd444ef06e550e55d1ef65da7bd6c0166292ac
sleesm <sleepingsm813@gmail.com>
2022-10-05 23:39:15 +0900

    [add] create controller pakage & HelloController class for hello view page

--- src/main/java/hello/hellospring/controller/HelloConroller.java
+++ src/main/java/hello/hellospring/controller/HelloConroller.java
@@ -1,2 +1,15 @@
-package hello.hellospring.controller;public class HelloConroller {
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloConroller {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
 }
