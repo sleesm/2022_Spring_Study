ca345de67e33c81183d999df95b418b6cc0b9e95
sleesm <sleepingsm813@gmail.com>
2023-01-06 20:45:43 +0900

    [update] using constructor injection for DIO & change constructor injection to using Spring container

--- core-principle-of-spring/src/main/java/hello/core/MemberApp.java
+++ core-principle-of-spring/src/main/java/hello/core/MemberApp.java
@@ -4,11 +4,21 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+
+        /** 생성자 주입 */
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        /** 스프링 컨테이너 사용 */
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
