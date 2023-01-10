f34c6a728980597c783b684816a9284da0d02936
sleesm <sleepingsm813@gmail.com>
2023-01-10 19:52:18 +0900

    [add & update] create/update testing code for @Configuration to check singleton

--- core-principle-of-spring/src/main/java/hello/core/AppConfig.java
+++ core-principle-of-spring/src/main/java/hello/core/AppConfig.java
@@ -15,20 +15,38 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    //@Bean memberService -> new MemoryMemberRepository()
+    //@Bean orderService -> new MemoryMemberRepository()
+
+    //예상값
+    //call AppConfig.memberService
+    //call AppConfig.memberRepository
+    //call AppConfig.orderService
+    //call AppConfig.memberRepository
+    //call AppConfig.memberRepository
+
+    //실제값
+    //call AppConfig.memberService
+    //call AppConfig.memberRepository
+    //call AppConfig.orderService
+
     /** 생성자 주입 -> Spring */
 
     @Bean
     public MemberService memberService(){
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public  OrderService orderService(){
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
