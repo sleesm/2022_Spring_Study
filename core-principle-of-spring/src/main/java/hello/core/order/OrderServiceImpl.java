18cb71d6dd2997b239aa1cabd69266b116fb1a4c
sleesm <sleepingsm813@gmail.com>
2023-01-13 21:43:09 +0900

    [add & update] create new annotation for Qualifier and use it in OrderServiceImpl class's constructor

--- core-principle-of-spring/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core-principle-of-spring/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
@@ -43,7 +44,7 @@ public class OrderServiceImpl implements OrderService{
 */
 
     //생성자 주입. 생성자 1개라서 @Autowired 생략 가능
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         System.out.println("1. OrderServiceImpl.OrderServiceImpl");
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
