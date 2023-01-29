c987b6de580a0029b0077301b73042522802e1af
sleesm <sleepingsm813@gmail.com>
2023-01-29 21:12:01 +0900

    [add] create Album, Book, Movie class extending Item class

---
+++ spring-boot-with-jpa1/src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+
+@Embeddable
+@Getter
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    //protected로 해놓으면 JPA 특성상 만들어둔 것이고 건들이지 X.
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+}
