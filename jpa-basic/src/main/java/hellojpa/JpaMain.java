a002f2b4e8f89f57444e5f914259284eee97cf8b
sleesm <sleepingsm813@gmail.com>
2023-02-20 21:18:04 +0900

    [update] add testing code of Persistence Context

--- jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -16,20 +16,39 @@ public class JpaMain {
         tx.begin();
 
         try {
-//            Member member = new Member();
-//            member.setId(1L);
-//            member.setName("HelloA");
-//            em.persist(member);
+/*            //바영속
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("HelloJPA");
 
-//            Member findMember = em.find(Member.class, 1L);
-//            System.out.println("findMember = " + findMember.getName());
-//            findMember.setName("HelloJPA");
+            //영속
+            em.persist(member);*/
 
-            List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
+/*            //1차 캐시
+            Member findMember1 = em.find(Member.class, 1L);
+            Member findMember2 = em.find(Member.class, 1L);
 
-            for (Member member : result) {
-                System.out.println("member = " + member.getName());
-            }
+            //동일성 보장
+            System.out.println(findMember1 == findMember2);*/
+
+            //쓰기 지연
+/*            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
+
+            em.persist(member1);
+            em.persist(member2);
+
+            System.out.println("===================================");*/
+
+/*            //변경 감지
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZ");*/
+
+            //플러쉬
+            Member member = new Member(200L, "member200");
+            em.persist(member);
+            em.flush();
+            System.out.println("===================================");
 
             tx.commit();
         }catch (Exception e){
