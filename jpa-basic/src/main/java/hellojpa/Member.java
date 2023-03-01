221fbf4c0d95d1aec35c4c79b206bed0425973fc
sleesm <sleepingsm813@gmail.com>
2023-03-01 21:14:27 +0900

    [update] add @Embedded field, Set<String> field, List<AddressEntity> field for testing value type in Member class

--- jpa-basic/src/main/java/hellojpa/Member.java
+++ jpa-basic/src/main/java/hellojpa/Member.java
@@ -3,9 +3,7 @@ package hellojpa;
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Entity
 //@TableGenerator(name = "MEMBER_SEQ_GENERATOR", table = "MY_SEQUENCES", pkColumnValue = "MEMBER_SEQ", allocationSize = 1)
@@ -20,11 +18,57 @@ public class Member extends BaseEntity{
     @Column(name = "USERNAME")
     private String username;
 
+
+    @Embedded
+    private Address homeAddress;
+
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns =
+        @JoinColumn(name = "MEMBER_ID")
+    )
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
+
+/*    @ElementCollection
+    @CollectionTable(name = "ADDRESS",joinColumns =
+        @JoinColumn(name = "MEMBER_ID")
+    )
+    private List<Address> addressHistory = new ArrayList<>();*/
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "MEMBER_ID")
+    private List<AddressEntity> addressHistory = new ArrayList<>();
+
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    public Set<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public void setFavoriteFoods(Set<String> favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
+    }
+
+    public List<AddressEntity> getAddressHistory() {
+        return addressHistory;
+    }
+
+    public void setAddressHistory(List<AddressEntity> addressHistory) {
+        this.addressHistory = addressHistory;
+    }
+
+/*
 //    @Column(name = "TEAM_ID")
 //    private Long teamId;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "TEAM_ID")
     private Team team;
+*/
 
 /*    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "LOCKER_ID")
@@ -50,7 +94,7 @@ public class Member extends BaseEntity{
         this.username = username;
     }
 
-    public Team getTeam() {
+/*    public Team getTeam() {
         return team;
     }
 
@@ -62,7 +106,7 @@ public class Member extends BaseEntity{
     public void changeTeam(Team team) {
         this.team = team;
         team.getMembers().add(this);
-    }
+    }*/
 
     /*    private Integer age;
 
