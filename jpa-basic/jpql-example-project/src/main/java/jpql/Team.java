79be4fe8850cd87adea16576aaf58e2c3b121411
sleesm <sleepingsm813@gmail.com>
2023-03-04 21:09:40 +0900

    [update] setting @BatchSize in Team class

--- jpa-basic/jpql-example-project/src/main/java/jpql/Team.java
+++ jpa-basic/jpql-example-project/src/main/java/jpql/Team.java
@@ -1,5 +1,7 @@
 package jpql;
 
+import org.hibernate.annotations.BatchSize;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +12,7 @@ public class Team {
     private Long id;
     private String name;
 
+    @BatchSize(size = 100)
     @OneToMany(mappedBy = "team")
     private List<Member> members = new ArrayList<>();
 
